d053979d4282c23cb6c43ff5e61a23ac088b3d0d
GreenTGreenT <s5901012630091@email.kmutnb.ac.th>
2019-01-12 01:07:58 +0700

    can calculate

--- calGPA.py
+++ calGPA.py
@@ -1,6 +1,8 @@
 import csv
 
 data = []
+result = []
+total_credit = []
 grade = {"A": 4,
          "B+": 3.5,
          "B": 3,
@@ -11,12 +13,12 @@ grade = {"A": 4,
          "F": 0}
 
 def read_csv():
-    with open('GPA.csv', 'r') as f:
+    with open('grade.csv', 'r') as f:
         reader = csv.reader(f)
         for i in reader:
             data.append(i) 
                   
-'''def grade_num(num):
+def grade_num(num):
     grade = {"A": 4,
              "B+": 3.5,
              "B": 3,
@@ -25,7 +27,7 @@ def read_csv():
              "D+": 1.5,
              "D": 1,
              "F": 0}
-    return grade[num]'''
+    return grade[num]
 
 def edit_csv():
     read_csv()
@@ -42,49 +44,47 @@ def edit_csv():
 
 def insert_csv():
     read_csv()
-    #print(data)
-    print("Please enter Q when you finish")
-    while True:
+    print("If you want to save, please enter S \n if you want to exit, please enter Q")
+    num_subject = int(input("Enter total number of your subjects: "))
+    for i in range(num_subject):        
+        term = input("Enter your term number: ")
+           
+        subject = input("Enter your subject: ")
+           
+
+        credit = input("Enter your credit: ")
         
-        term = input("Enter your term number: ").upper()
-        subject = input("Enter your subject: ").upper()
-        credit = input("Enter your credit: ").upper()
+
+
         grade = input("Enter your grade: ").upper()
-       
-        '''if term == "Q":
-            break
-        elif subject == "Q":
-            break
-        elif credit == "Q":
-            break
-        elif grade == "Q":
-            break'''
         data.append([term, subject, credit, grade])
-       
-        break         
-    #print(data)   
-    save = input("If you want to save, please enter S: ").upper 
+              
+    save = input("Do you want to save?: ").upper()
     if save == "S":
-        save_csv()
+        save_csv(term)
+    elif save == "Q":
+        exit()
 
-def save_csv():
-    with open('GPA.csv', 'w') as f:
-            writer = csv.writer(f)
-            writer.writerows(data) 
-
-def calculation():
-    read_csv()
-    term = input("Please enter you term: ")
+    
+def save_csv(term):
+    with open('grade.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+    calculation(term)
+    
+def calculation(term):
     sum_multiple = 0
     sum_credit = 0
+    to_result = 0
+    
     for i in range(len(data)):
         if data[i][0] == term:
-            if data[i][2].isnumeric():
-                sum_multiple += int(data[i][2]) * grade.get(data[i][3])
+            if data[i][2] is not None or data[i][3] is not None:
+                sum_multiple += int(data[i][2]) * grade_num(data[i][3])
                 sum_credit += int(data[i][2]) 
-                result = sum_multiple / sum_credit
-    #print(sum_multiple)
-    print(result)
+                to_result = sum_multiple / sum_credit
+    print("You GPA is ", "%.2f " % to_result)
+                 
         
 def main():
     print("Please choose your mode \n Edit(E) Insert(I)")
@@ -95,6 +95,8 @@ def main():
         insert_csv()
     elif mode == "C":
         calculation()
+    elif mode == "S":
+        save_csv()
     else:
         print("Please try again")
         main()
