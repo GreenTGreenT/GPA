ab2903307a08f5b0c572861b11da53d5d2906382
GreenTGreenT <s5901012630091@email.kmutnb.ac.th>
2019-01-12 02:16:01 +0700

    complete

--- calGPA.py
+++ calGPA.py
@@ -1,4 +1,5 @@
 import csv
+import operator
 
 data = []
 result = []
@@ -17,30 +18,28 @@ def read_csv():
         reader = csv.reader(f)
         for i in reader:
             data.append(i) 
-                  
-def grade_num(num):
-    grade = {"A": 4,
-             "B+": 3.5,
-             "B": 3,
-             "C+": 2.5,
-             "C": 2,
-             "D+": 1.5,
-             "D": 1,
-             "F": 0}
-    return grade[num]
-
+        #data = sorted(data, key = operator.itemgetter(0))
+ 
 def edit_csv():
     read_csv()
-    ask = int(input("Enter term that you want to edit: "))
-    subject = input("Enter your subject: ")
-    credit = input("Enter your credit: ")
-    grade = input("Enter your grade: ")
+    term = input("Enter term that you want to edit: ")
+
+    print("----------------------------------------------------------------------------------")
+    #print("|order|", "|term|", "|                     subject                    |", "|credit|", "|grade|")
     for i in range(len(data)):
-        if data[ask] == data[0]:
-            data[ask][1] = subject
-            data[ask][2] = credit
-            data[ask][3] = grade
-            print(data[ask][3])
+        if data[i][0] == term:
+            print("|  ", i, "  |", data[i][0], "|", data[i][1], "|", data[i][2], "|", data[i][3], "|")
+    print("----------------------------------------------------------------------------------")
+    edit_point = int(input("What's number you want to edit: "))
+    for i in range(len(data)):
+        if edit_point == i:
+            subject = input("Enter your subject: ")
+            credit = input("Enter your credit: ")
+            grade = input("Enter your grade: ").upper()
+            data[int(i)][1] = subject
+            data[int(i)][2] = credit
+            data[int(i)][3] = grade
+            save_csv(term)
 
 def insert_csv():
     read_csv()
@@ -67,6 +66,7 @@ def insert_csv():
 
     
 def save_csv(term):
+    #print(data)
     with open('grade.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerows(data)
@@ -79,10 +79,10 @@ def calculation(term):
     
     for i in range(len(data)):
         if data[i][0] == term:
-            if data[i][2] is not None or data[i][3] is not None:
-                sum_multiple += int(data[i][2]) * grade_num(data[i][3])
-                sum_credit += int(data[i][2]) 
-                to_result = sum_multiple / sum_credit
+            #print(grade.get(data[i][3]))
+            sum_multiple += int(data[i][2]) * grade.get(data[i][3])
+            sum_credit += int(data[i][2]) 
+            to_result = sum_multiple / sum_credit
     print("You GPA is ", "%.2f " % to_result)
                  
         
